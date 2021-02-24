d0abfd1cd91fc193e60f8510ca6fba980940ad4a
Joel Louzado <jlouzado@gmail.com>
2021-02-24 16:37:29 +0530

    feat(landingPage): use svg logo

--- client/edevi/src/LandingPageModule.js
+++ client/edevi/src/LandingPageModule.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
-import logoSmall from './images/edevi.png';
-import logoMedium from './images/edevi@2x.png';
-import logoLarge from './images/edevi@3x.png';
+import deviLogo from './images/edevi.svg';
 import Button from '@material-ui/core/Button';
 import './css/App.css';
 
@@ -23,18 +21,9 @@ function LandingPageModule(props) {
 
   return (
     <div className={'LandingPageModule'}>
-            <img alt="devi logo" className='AppLogoImage'
-              srcSet={`
-                ${logoSmall} 183w,
-                ${logoMedium} 366w,
-                ${logoLarge} 549w
-              `}
-              sizes={`
-                (max-width: 400px) 183px,
-                (max-width: 900px) 366px,
-                (min-width: 901px) 549px
-              `}
-              src={logoMedium}
+            <img alt="devi logo"
+              className='AppLogoImage'
+              src={deviLogo}
             />
             <div className={'landingPageHeader'} variant="h2">The path to infinity begins here </div> 
             <Button className={'EnterTempleButton'} onClick={()=>{props.history.push('Glitch')}}> All may enter </Button>
